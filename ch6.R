188ad346c7663d664f9b16b5b5d267d7c806b0b6
Jacob Lahne <jlahne@vt.edu>
2021-05-31 15:27:44 -0400

    Finished chapter 6

--- ch6.R
+++ ch6.R
@@ -206,15 +206,291 @@ y_test <- as.array(labels)
 model_glove %>% evaluate(x_test, y_test)
 model_noglove %>% evaluate(x_test, y_test)  
   
+# RNNs - reset R here
 
+rm(list = ls()); gc()
+library(keras)
+library(tidyverse)
 
+timesteps <- 100
+input_features <- 32
+output_features <- 64
 
+random_array <- function(dim){
+  array(runif(prod(dim)), dim = dim)
+}
 
+inputs <- random_array(dim = c(timesteps, input_features))
+state_t <- rep_len(0, length = output_features)
 
+W <- random_array(dim = c(output_features, input_features))
+U <- random_array(dim = c(output_features, output_features))
+b <- random_array(dim = c(output_features, 1))
 
+output_sequence <- array(0, dim = c(timesteps, output_features))
+for(i in 1:nrow(inputs)){
+  input_t <- inputs[i, ]
+  output_t <- tanh(
+    as.numeric(
+      (W %*% input_t) + (U %*% state_t) + b
+    )
+  )
+  output_sequence[i,] <- as.numeric(output_t)
+  state_t <- output_t
+}
+output_sequence
 
+rm(list = ls()); gc()
 
+max_features <- 10000
+maxlen = 500
+batch_size = 32
 
+imdb <- dataset_imdb(num_words = max_features)
+input_train <- imdb$train$x
+y_train <- imdb$train$y
+input_test <- imdb$test$x
+y_test <- imdb$test$y
+
+input_train <- pad_sequences(input_train, maxlen = maxlen)
+input_test <- pad_sequences(input_test, maxlen = maxlen)
+dim(input_train)
+dim(input_test)
+
+model_rnn <- keras_model_sequential() %>% 
+  layer_embedding(input_dim = max_features, output_dim = 32) %>% 
+  layer_simple_rnn(units = 32) %>% 
+  layer_dense(units = 1, activation = "sigmoid") %>% 
+  compile(
+    optimizer = "rmsprop",
+    loss = "binary_crossentropy",
+    metrics = "acc"
+  )
+model_rnn
+
+history <- model_rnn %>% 
+  fit(
+    input_train,
+    y_train,
+    epochs = 10,
+    batch_size = 128,
+    validation_split = 0.2
+  )
+evaluate(model_rnn, input_test, y_test)
+
+model_lstm <- keras_model_sequential() %>% 
+  layer_embedding(input_dim = max_features, output_dim = 32) %>% 
+  layer_lstm(units = 32) %>% 
+  layer_dense(units = 1, activation = "sigmoid") %>% 
+  compile(
+    optimizer = "rmsprop",
+    loss = "binary_crossentropy",
+    metrics = "acc"
+  )
+
+history <- model_lstm %>% 
+  fit(
+    input_train,
+    y_train,
+    epochs = 10,
+    batch_size = 128,
+    validation_split = 0.2
+  )
+  
+evaluate(model_lstm, input_test, y_test)
+
+# Advanced RNNs (and generators)--reset R here
+
+library(keras)
+library(tidyverse)
+
+data_dir <- "~/Documents/R/Deep Learning with R/jena_climate"  
+fname <- file.path(data_dir, "jena_climate_2009_2016.csv")  
+data <- read_csv(fname)  
+
+glimpse(data)
+
+ggplot(data, aes(x = 1:nrow(data), y = `T (degC)`)) + geom_line()
+ggplot(data[1:1440, ], aes(x = 1:1440, y = `T (degC)`)) + geom_line()
+
+sequence_generator <- function(start){
+  value <- start - 1
+  function(){
+    value <<- value + 1
+    value
+  }
+}
+
+gen <- sequence_generator(10)
+gen()
+gen()
+gen()
+gen2 <- sequence_generator(10)
+gen()
+gen2()
+gen2
+
+data <- data.matrix(data[, -1])
+train_data <- data[1:200000, ]
+mean <- apply(train_data, 2, mean)
+std <- apply(train_data, 2, sd)
+data <- scale(data, center = mean, scale = std)
+
+# generator <- function(data, lookback, delay, min_index, max_index, shuffle = FALSE, batch_size = 128, step = 6){
+#   
+#   if(is.null(max_index))
+#     max_index <- nrow(data) - delay - 1
+#   i <- min_index + lookback
+#   
+#   function(){
+#     if(shuffle){
+#       rows <- sample(c((min_index + lookback):max_index), size = batch_size)
+#     } else{
+#       if(i + batch_size >= max_index)
+#         i <<- min_index + lookback
+#       rows <- c(i:min(i + batch_size - 1, max_index))
+#       i <<- i + length(rows)
+#     }
+#     
+#     samples <- array(0, dim = c(length(rows), lookback / step, dim(data)[[-1]]))
+#     targets <- array(0, dim = length(rows))
+#     
+#     for(j in 1:length(rows)){
+#       indices <- seq(rows[[j]] - lookback, rows[[j]] - 1, length.out = dim(samples)[[2]])
+#       samples[j, , ] <- data[indices, ]
+#       targets[[j]] <- data[rows[[j]] + delay, 2]
+#     }
+#     
+#     list(samples, targets)
+#     
+#   }
+#   
+# }
+
+generator <- function(data, lookback, delay, min_index, max_index,
+                      shuffle = FALSE, batch_size = 128, step = 6) {
+  if (is.null(max_index))
+    max_index <- nrow(data) - delay - 1
+  i <- min_index + lookback
+  function() {
+    if (shuffle) {
+      rows <- sample(c((min_index+lookback):max_index), size = batch_size)
+    } else {
+      if (i + batch_size >= max_index)
+        i <<- min_index + lookback
+      rows <- c(i:min(i+batch_size-1, max_index))
+      i <<- i + length(rows)
+    }
+    
+    samples <- array(0, dim = c(length(rows),
+                                lookback / step,
+                                dim(data)[[-1]]))
+    targets <- array(0, dim = c(length(rows)))
+    
+    for (j in 1:length(rows)) {
+      indices <- seq(rows[[j]] - lookback, rows[[j]]-1,
+                     length.out = dim(samples)[[2]])
+      samples[j,,] <- data[indices,]
+      targets[[j]] <- data[rows[[j]] + delay,2]
+    }           
+    list(samples, targets)
+  }
+}
+
+lookback <- 1440
+step <- 6
+delay <- 144
+batch_size <- 128
+
+train_gen <- generator(data, delay = delay, lookback = lookback, min_index = 1, max_index = 200000, shuffle = TRUE, step = step, batch_size = batch_size)
+val_gen <- generator(data, lookback = lookback, delay = delay, min_index = 200001, max_index = 300000, step = step, batch_size = batch_size, shuffle = FALSE)
+test_gen <- generator(data, lookback = lookback, delay = delay, min_index = 300001, max_index = NULL, step = step, batch_size = batch_size, shuffle = FALSE)
+
+val_steps <- (300000 - 200001 - lookback) / batch_size
+test_steps <- (nrow(data) - 300001 - lookback) / batch_size
+
+evaluate_naive_method <- function(){
+  batch_maes <- c()
+  for(step in 1:val_steps){
+    c(samples, targets) %<-% val_gen()
+    preds <- samples[, dim(samples)[[2]], 2]
+    mae <- mean(abs(preds - targets))
+    batch_maes <- c(batch_maes, mae)
+  }
+  print(mean(batch_maes))
+}
+
+evaluate_naive_method()
+
+model_temp_simple <- keras_model_sequential() %>% 
+  layer_flatten(input_shape = c(lookback / step, dim(data)[-1])) %>% 
+  layer_dense(units = 32, activation = "relu") %>% 
+  layer_dense(units = 1) %>% 
+  compile(
+    optimizer = "rmsprop",
+    loss = "mae"
+  )
+
+history <- model_temp_simple %>% 
+  fit_generator(
+    train_gen,
+    steps_per_epoch = 500,
+    epochs = 20,
+    validation_data = val_gen,
+    validation_steps = val_steps
+  )
+
+model_temp_gru <- keras_model_sequential() %>%
+  layer_gru(units = 32, input_shape = list(NULL, dim(data)[[-1]])) %>% 
+  layer_dense(units = 1) %>% 
+  compile(
+    optimizer = optimizer_rmsprop(),
+    loss = loss_mean_absolute_error
+  )
+
+history <- model_temp_gru %>% 
+  fit(
+    train_gen,
+    steps_per_epoch = 500,
+    epochs = 20,
+    validation_data = val_gen,
+    validation_steps = val_steps
+  )
+
+# Bidirectional RNNs -- restart R here
+
+rm(list = ls()); gc()
+library(keras)
+library(tidyverse)
+
+max_features <- 10000
+maxlen <- 500
+
+c(c(x_train, y_train), c(x_test, y_test)) %<-% dataset_imdb(num_words = max_features)
+x_train <- pad_sequences(x_train, maxlen)
+x_test <- pad_sequences(x_test, maxlen)
+
+model_imdb_bidirectional <- keras_model_sequential() %>% 
+  layer_embedding(input_dim = max_features, output_dim = 32) %>% 
+  bidirectional(
+    layer_lstm(units = 32)
+  ) %>% 
+  layer_dense(units = 1, activation = "sigmoid") %>% 
+  compile(
+    optimizer = "rmsprop",
+    loss = "binary_crossentropy",
+    metrics = "acc"
+  )
+
+history <- model_imdb_bidirectional %>% 
+  fit(
+    x_train,
+    y_train,
+    epochs = 10,
+    batch_size = 128,
+    validation_split = 0.2
+  )
+  
 
 
 
